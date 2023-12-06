ee5dd561c058406da36f755ea6bce7fdf5d4c414
sylwia <sekula.syl@gmail.com>
2023-12-06 20:18:15 +0100

    update

--- scripts/data/prepare_data.py
+++ scripts/data/prepare_data.py
@@ -1,5 +1,6 @@
-import os
 import pandas as pd
+
+from scripts.settings import *
 
 
 def select_station_and_resample_dataset(dataframe: pd.DataFrame, station_column_name: str, selected_station: str,
@@ -17,55 +18,49 @@ def select_station_and_resample_dataset(dataframe: pd.DataFrame, station_column_
     dataframe = dataframe[dataframe[station_column_name] == selected_station].copy()
     dataframe[date_time_column_name] = pd.to_datetime(dataframe[date_time_column_name])
     dataframe.set_index(date_time_column_name, inplace=True)
+    dataframe = dataframe.drop(station_column_name, axis=1)
     dataframe = dataframe.resample(resample_label).mean()
     return dataframe
 
 
-def create_statistical_train_dateset(dataframe: pd.DataFrame, end_date: str, columns_to_drop: list,
-                                     train_dataset_columns: list) -> pd.DataFrame:
+def create_train_test_dateset(dataframe: pd.DataFrame, n_periods: int, columns_to_drop: list,
+                              train_dataset_columns: list) -> pd.DataFrame:
     """
     Prepares the training dataset for statistical models for time series forecasting
     by filtering data up to the specified end date, dropping selected columns,
     and renaming the columns as per the provided list.
     :param dataframe: pd.DataFrame, the dataset
-    :param end_date: str, the end date until which the data will be filtered
+    :param n_periods: int, the number of periods used for creating the train and test dataset.
     :param columns_to_drop: list, the list of columns to drop
     :param train_dataset_columns: list, the List of column names for the training dataset
     :return: pd.DataFrame, prepared training dataset
     """
-    dataframe_train = dataframe[(dataframe.index <= end_date)].copy()
-    dataframe_train = dataframe_train.drop(columns_to_drop, axis=1)
-    dataframe_train.reset_index(inplace=True)
-    dataframe_train.columns = train_dataset_columns
-    return dataframe_train
-
+    dataframe = dataframe.drop(columns_to_drop, axis=1)
+    dataframe.reset_index(inplace=True)
+    dataframe.columns = train_dataset_columns
+    dataframe_train, dataframe_test = dataframe.iloc[:-n_periods], dataframe.iloc[-n_periods:]
+    return dataframe_train, dataframe_test
 
 
 def main():
-    current_directory = os.getcwd()
-    input_filename = 'sp_air_quality.csv'
-    output_resampled_filename = 'df_resampled.csv'
-    output_statistical_train_filename = 'df_stat_train'
-    input_path = os.path.join(current_directory, input_filename)
-    output_resampled_path = os.path.join(current_directory, output_resampled_filename)
-    output_train_path = os.path.join(current_directory, output_statistical_train_filename)
     df = pd.read_csv(input_path)
     station_column_name = 'Station'
     selected_station = 'Taubaté'
     date_time_column_name = 'Datetime'
     resample_label = "M"
-    end_date = pd.to_datetime('2019-08-31')
+    n_periods = 12
     columns_to_drop = ['Benzene', 'CO', 'PM2.5', 'NO2', 'O3', 'SO2', 'Toluene', 'TRS']
-    train_dataset_columns = ['ds', 'y']
+    dataset_columns = ['ds', 'y']
     # resample the dataset
-    df = select_station_and_resample_dataset(df, station_column_name, selected_station, date_time_column_name, resample_label)
+    df = select_station_and_resample_dataset(df, station_column_name, selected_station, date_time_column_name,
+                                             resample_label)
     # save the resampled dataset
     df.to_csv(output_resampled_path, index=False)
     # prepare the train dataset for statistical models
-    df_stat_train = create_statistical_train_dateset(df, end_date, columns_to_drop, train_dataset_columns)
+    df_train, df_test = create_train_test_dateset(df, n_periods, columns_to_drop, dataset_columns)
     # save the train dataset for statistical models
-    df_stat_train.to_csv(output_train_path)
-
+    df_train.to_csv(output_train_path, index=False)
+    df_test.to_csv(output_test_path, index=False)
 
 
 if __name__ == '__main__':

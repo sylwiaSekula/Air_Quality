2a707518ccbbb53b0b89c49c1395a8dfe9b8d6b0
sylwia <sekula.syl@gmail.com>
2023-12-19 18:45:11 +0100

    add XGBoost Regressor

--- scripts/data/prepare_data.py
+++ scripts/data/prepare_data.py
@@ -24,7 +24,7 @@ def select_station_and_resample_dataset(dataframe: pd.DataFrame, station_column_
 
 
 def create_train_test_dateset(dataframe: pd.DataFrame, n_periods: int, columns_to_drop: list,
-                              train_dataset_columns: list) -> pd.DataFrame:
+                              train_dataset_columns: list, num_lags: int, predicted_column: str) -> pd.DataFrame:
     """
     Prepares the training dataset for statistical models for time series forecasting
     by filtering data up to the specified end date, dropping selected columns,
@@ -33,12 +33,26 @@ def create_train_test_dateset(dataframe: pd.DataFrame, n_periods: int, columns_t
     :param n_periods: int, the number of periods used for creating the train and test dataset.
     :param columns_to_drop: list, the list of columns to drop
     :param train_dataset_columns: list, the List of column names for the training dataset
+    :param num_lags: int, the number of lags for the machine learning dataset
+    :param predicted_column: str, the name of the predicted column
     :return: pd.DataFrame, prepared training dataset
     """
-    dataframe = dataframe.drop(columns_to_drop, axis=1)
-    dataframe.reset_index(inplace=True)
-    dataframe.columns = train_dataset_columns
-    dataframe_train, dataframe_test = dataframe.iloc[:-n_periods], dataframe.iloc[-n_periods:]
+    if num_lags > 0:
+        for i in range(1, num_lags + 1):
+            dataframe[f'lag_{i}'] = dataframe[predicted_column].shift(i)
+        dataframe = dataframe.drop(dataframe.index[:12])
+        missing_values = dataframe.isnull().sum()
+        columns_with_missing = missing_values[missing_values > 0]
+        dataframe = dataframe.drop(columns=columns_with_missing.index)
+        dataframe.reset_index(inplace=True)
+        dataframe.rename(columns={'PM10': 'y', 'Datetime': 'ds'}, inplace=True)
+        dataframe_train, dataframe_test = dataframe.iloc[:-n_periods], dataframe.iloc[-n_periods:]
+
+    else:
+        dataframe = dataframe.drop(columns_to_drop, axis=1)
+        dataframe.reset_index(inplace=True)
+        dataframe.columns = train_dataset_columns
+        dataframe_train, dataframe_test = dataframe.iloc[:-n_periods], dataframe.iloc[-n_periods:]
     return dataframe_train, dataframe_test
 
 
@@ -47,19 +61,23 @@ def main():
     station_column_name = 'Station'
     selected_station = 'Taubaté'
     resample_label = "M"
-    n_periods = 12
     columns_to_drop = ['Benzene', 'CO', 'PM2.5', 'NO2', 'O3', 'SO2', 'Toluene', 'TRS']
     dataset_columns = ['ds', 'y']
+    num_lags = 12
     # resample the dataset
     df = select_station_and_resample_dataset(df, station_column_name, selected_station, date_time_column_name,
                                              resample_label)
     # save the resampled dataset
     df.to_csv(output_resampled_path, index=True)
     # prepare the train dataset for statistical models
-    df_train, df_test = create_train_test_dateset(df, n_periods, columns_to_drop, dataset_columns)
+    #df_train, df_test = create_train_test_dateset(df, n_periods, columns_to_drop, dataset_columns)
+    df_train_ml, df_test_ml = create_train_test_dateset(df, n_periods, columns_to_drop, dataset_columns,
+                                                        num_lags=num_lags, predicted_column="PM10")
     # save the train dataset for statistical models
-    df_train.to_csv(output_train_path, index=False)
-    df_test.to_csv(output_test_path, index=False)
+    # df_train.to_csv(output_train_path, index=False)
+    # df_test.to_csv(output_test_path, index=False)
+    df_train_ml.to_csv(output_train_ml_filename, index=False)
+    df_test_ml.to_csv(output_test_ml_filename, index=False)
 
 
 if __name__ == '__main__':
--- scripts/models/prepare_and_train_models.py
+++ scripts/models/prepare_and_train_models.py
@@ -1,45 +1,62 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import xgboost as xgb
 from prophet import Prophet
+from sklearn.metrics import mean_absolute_error
 from sktime.forecasting.arima import AutoARIMA
 from sktime.forecasting.exp_smoothing import ExponentialSmoothing
 
 from scripts.settings import *
 
 
-def read_data(train_path: str, resampled_path: str, date_time_column_name: str, datestamp_column: str) \
-        -> (pd.DataFrame, pd.DataFrame):
+def read_data(train_path: str, train_ml_path: str, test_ml_path: str, resampled_path: str, date_time_column_name: str,
+              datestamp_column: str) -> (pd.DataFrame, pd.DataFrame):
     """
     Read and prepare time series data from CSV files.
     :param train_path: str, the filepath to the training data CSV file
+    :param train_ml_path: str, the filepath to the training data for a machine learning model CSV file
+    :param test_ml_path: str, the filepath to the test data for a machine learning model CSV file
     :param resampled_path: str, the filepath to the resampled data CSV file
     :param date_time_column_name: str, the name of the column in the resampled data CSV file that represents the date
     :param datestamp_column: str, the name of the column in the training data CSV file that represents the date
     :return:
         - df_train: pd.DataFrame, the processed training data
+        - df_train_ml: pd.DataFrame, the processed training data for machine learning
+        - df_test_ml: pd.DataFrame, the processed test data for machine learning
         - df_resampled: pd.DataFrame, the processed resampled data
     """
-    df_train = pd.read_csv(train_path)
+
     df_resampled = pd.read_csv(resampled_path)
     df_resampled.set_index(pd.to_datetime(df_resampled[date_time_column_name]), inplace=True)
+    df_train = pd.read_csv(train_path)
     df_train[datestamp_column] = pd.to_datetime(df_train[datestamp_column])
     df_train.set_index(datestamp_column, inplace=True)
     df_train.index.freq = "M"
-    return df_train, df_resampled
+    df_train_ml = pd.read_csv(train_ml_path)
+    df_train_ml[datestamp_column] = pd.to_datetime(df_train_ml[datestamp_column])
+    df_train_ml.set_index(datestamp_column, inplace=True)
+    df_test_ml = pd.read_csv(test_ml_path)
+    df_test_ml[datestamp_column] = pd.to_datetime(df_test_ml[datestamp_column])
+    df_test_ml.set_index(datestamp_column, inplace=True)
+    return df_train, df_train_ml, df_test_ml, df_resampled
 
 
-def fit_model(df_train: pd.DataFrame, model, predicted_column: str, datestamp_column: str) -> tuple:
+def fit_model(df_train: pd.DataFrame, df_train_ml: pd.DataFrame, df_test_ml: pd.DataFrame, model, predicted_column: str,
+              datestamp_column: str) -> tuple:
     """
     Fit time series forecasting models and make predictions.
     :param df_train: (pd.DataFrame), the DataFrame with training data, indexed by the specified datestamp_column
+    :param df_train_ml: pd.DataFrame, the DataFrame with machine learning training data
+    :param df_test_ml: pd.DataFrame, the DataFrame with machine learning test data
     :param model: the time series forecasting model (AutoARIMA, ExponentialSmoothing, Prophet)
     :param predicted_column: (str), the column name of the target variable
     :param datestamp_column: (str), the name of the column representing the date or timestamp in the data
     :return: Tuple: Model-specific output:
     - ExponentialSmoothing or AutoARIMA: Tuple of fitted values and predictions.
     - Prophet: Tuple of fitted values and forecasted values.
+    - XGBRegressor: Tuple of fitted model and predicted values.
     Raises:
-    - TypeError: If the model type is not one of (ExponentialSmoothing, AutoARIMA, or Prophet).
+    - TypeError: If the model type is not one of (ExponentialSmoothing, AutoARIMA, Prophet, or XGBRegressor).
     """
     if isinstance(model, (AutoARIMA, ExponentialSmoothing)):
         y_train = df_train[predicted_column]
@@ -56,37 +73,63 @@ def fit_model(df_train: pd.DataFrame, model, predicted_column: str, datestamp_co
         forecast_prophet[datestamp_column] = pd.to_datetime(forecast_prophet[datestamp_column])
         forecast_prophet.set_index(datestamp_column, inplace=True)
         fitted_prophet = forecast_prophet[(forecast_prophet.index <= pd.to_datetime('2019-10-01'))]
-        forecast_prophet = forecast_prophet[(forecast_prophet.index >= pd.to_datetime('2019-09-01'))]
+        forecast_prophet = forecast_prophet[(forecast_prophet.index >= pd.to_datetime('2019-10-01'))]
         return fitted_prophet['yhat'], forecast_prophet['yhat']
+    elif isinstance(model, xgb.XGBRegressor):
+        y_train = df_train_ml[predicted_column]
+        X_train = df_train_ml.drop(predicted_column, axis=1)
+        X_test = df_test_ml.drop(predicted_column, axis=1)
+        y_fitted_model = model.fit(X_train, y_train)
+        y_pred_model = model.predict(X_test)
+        return y_fitted_model, y_pred_model
     else:
-        raise TypeError("Use one of (ExponentialSmoothing, AutoARIMA or Prophet) model type.")
+        raise TypeError("Use one of (ExponentialSmoothing, AutoARIMA,Prophet or XGBRegressor) model type.")
 
 
 def main():
     # read the data
-    df_train, df_resampled = read_data(output_train_path, output_resampled_path, date_time_column_name,
-                                       datestamp_column)
+    df_train, df_train_ml, df_test_ml, df_resampled = read_data(output_train_path, output_train_path_ml,
+                                                                output_test_path_ml, output_resampled_path,
+                                                                date_time_column_name, datestamp_column)
     # create models
     hw = ExponentialSmoothing(trend='add', seasonal='multiplicative', sp=12)
     arima = AutoARIMA(sp=12, d=0, max_p=2, max_q=2, suppress_warnings=True)
     prophet = Prophet(interval_width=0.9)
+    xgboost = xgb.XGBRegressor()
     models = {
         'HW': hw,
         'AutoARIMA': arima,
-        'Prophet': prophet
+        'Prophet': prophet,
+        'XGBoost': xgboost
     }
     # fit the models and plot
     for model_name, model in models.items():
         # fit the model and get fitted values and predictions
-        fitted, predicted = fit_model(df_train, model, predicted_column, datestamp_column)
-        plt.figure(figsize=(12, 8))
-        plt.plot(df_resampled['PM10'], label='actuals') # plot actual PM10 values from the resampled data
-        plt.plot(fitted, label='fitted') # plot fitted values from the model
-        plt.plot(predicted, label='predicted') # plot predicted values from the model
-        plt.title(model_name)
-        plt.legend()
-        plt.savefig(f'../plots/{model_name}.png', format='png')
-        plt.show()
+        fitted, predicted = fit_model(df_train, df_train_ml, df_test_ml, model, predicted_column, datestamp_column)
+        mae = mean_absolute_error(df_test_ml[predicted_column], predicted)
+        if model == xgboost:
+            # create the dataset with predicted values with datetime index
+            datetime_index = df_test_ml.index
+            predicted_df = pd.DataFrame(predicted, index=datetime_index, columns=['predicted_column'])
+            plt.figure(figsize=(12, 8))
+            plt.plot(df_resampled['PM10'], label='actuals')  # plot actual PM10 values from the resampled data
+            plt.plot(predicted_df['predicted_column'], label='predicted')  # plot predicted values from the model
+            plt.title(model_name)
+            plt.legend()
+            plt.savefig(f'../plots/{model_name}.png', format='png')
+            plt.show()
+            print(type(fitted), type(predicted))
+            print(model_name, mae)
+        else:
+            plt.figure(figsize=(12, 8))
+            plt.plot(df_resampled['PM10'], label='actuals')  # plot actual PM10 values from the resampled data
+            plt.plot(fitted, label='fitted')  # plot fitted values from the model
+            plt.plot(predicted, label='predicted')  # plot predicted values from the model
+            plt.title(model_name)
+            plt.legend()
+            plt.savefig(f'../plots/{model_name}.png', format='png')
+            plt.show()
+            print(model_name, mae)
 
 
 if __name__ == '__main__':
